1b79435a517bdd45bdf844908c31fde6b6f47d85
Juraj Lapcak <lapcakjuraj@gmail.com>
2021-03-05 23:30:59 +0100

    Implemented title screen

---
+++ Assets/Scripts/UI/Title Screen/TitleScreenManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+namespace LocationRPG
+{
+    // manages title screen
+    public class TitleScreenManager: VisualElement
+    {
+        private VisualElement _playButton;
+        private VisualElement _optionsButton;
+        private VisualElement _exitButton;
+        
+        public new class UxmlFactory : UxmlFactory<TitleScreenManager, UxmlTraits> { }
+        
+        public TitleScreenManager()
+        {
+            RegisterCallback<GeometryChangedEvent>(OnGeometryChange);
+        }
+
+        void OnGeometryChange(GeometryChangedEvent evt)
+        {
+            _playButton = this.Q("playButton");
+            _optionsButton = this.Q("optionsButton");
+            _exitButton = this.Q("exitButton");
+            
+            _playButton.RegisterCallback<ClickEvent>(ev => Play());
+            _optionsButton.RegisterCallback<ClickEvent>(ev => ShowOptions());
+            _exitButton.RegisterCallback<ClickEvent>(ev => Application.Quit());
+        }
+
+        void Play()
+        {
+            SceneManager.LoadScene(SceneTypes.World.ToString());
+        }
+
+        void ShowOptions()
+        {
+            
+        }
+    }
+}
