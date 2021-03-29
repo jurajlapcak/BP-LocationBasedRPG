a1d01ef7ce46584761d06e59c13e250d127fd8f2
Juraj Lapcak <lapcakjuraj@gmail.com>
2021-03-29 10:57:36 +0200

    Hotfix moving Skeleton

---
+++ Assets/Scripts/Scene/FightScene/FightSceneManager.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace LocationRPG
+
+{
+    public class FightSceneManager: MonoBehaviour
+    {
+        public void OnEnable()
+        {
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+
+        private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            GameObject monster = GameObject.Find("skeleton-interacted");
+            Debug.Log(monster.name);
+            monster.name = "Skeleton";
+
+            monster.transform.position = new Vector3(0,0,1);
+        }
+    }
+}
